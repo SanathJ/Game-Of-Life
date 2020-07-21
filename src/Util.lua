3d439ed7777a08d7e40ebac87cb42c2dfa4e098c
Sanath Jathanna <sanathjathanna@gmail.com>
2020-07-21 23:55:00 +0530

    removed unnecessary utility functions removed table.slice and copy

--- src/Util.lua
+++ src/Util.lua
@@ -10,21 +10,6 @@
 ]]
 
 --[[
-    Utility function for slicing tables, a la Python.
-
-    https://stackoverflow.com/questions/24821045/does-lua-have-something-like-pythons-slice
-]]
-function table.slice(tbl, first, last, step)
-    local sliced = {}
-  
-    for i = first or 1, last or #tbl, step or 1 do
-      sliced[#sliced+1] = tbl[i]
-    end
-  
-    return sliced
-end
-
---[[
     Renders the board
 ]]
 function renderBoard(cells)
@@ -41,26 +26,6 @@ function renderBoard(cells)
         end
     end
     love.graphics.setColor(255, 255, 255, 255)
-end
-
---[[
-    Handles (deep-)copying of tables
-    https://stackoverflow.com/a/26367080/11941952
-]]
-function copy(obj, seen)
-    if type(obj) ~= 'table' then
-        return obj
-    end
-    if seen and seen[obj] then
-        return seen[obj]
-    end
-    local s = seen or {}
-    local res = setmetatable({}, getmetatable(obj))
-    s[obj] = res
-    for k, v in pairs(obj) do
-        res[copy(k, s)] = copy(v, s)
-    end
-    return res
 end
 
 function neighbours(cells, x, y)
